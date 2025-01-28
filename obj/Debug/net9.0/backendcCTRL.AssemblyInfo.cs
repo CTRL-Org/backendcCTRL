0ca76183846ee2d8b693e5fcdf54cf07cb6d1923
Hiccupfish <122051041+Hiccupfish@users.noreply.github.com>
2025-01-28 20:37:13 +0200

    assembly info update

--- obj/Debug/net9.0/backendcCTRL.AssemblyInfo.cs
+++ obj/Debug/net9.0/backendcCTRL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backendcCTRL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5099c8483963ad56814cf66fc7d6501a1bf38249")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4130d9d4422e5bb4b2ffd0f93407052bd8ce783")]
 [assembly: System.Reflection.AssemblyProductAttribute("backendcCTRL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backendcCTRL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
